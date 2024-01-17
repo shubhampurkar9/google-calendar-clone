85b627980ede0f9ffcf149dfdc2dcc47a9510e79
Shubham <shubhampurkar9@gmail.com>
2024-01-17 11:57:30 +0530

    calendar

---
+++ src/components/Day.js
@@ -0,0 +1,60 @@
+import dayjs from "dayjs";
+import React, { useContext, useState, useEffect } from "react";
+import GlobalContext from "../context/GlobalContext";
+
+export default function Day({ day, rowIdx }) {
+  const [dayEvents, setDayEvents] = useState([]);
+  const {
+    setDaySelected,
+    setShowEventModal,
+    filteredEvents,
+    setSelectedEvent,
+  } = useContext(GlobalContext);
+
+  useEffect(() => {
+    const events = filteredEvents.filter(
+      (evt) =>
+        dayjs(evt.day).format("DD-MM-YY") === day.format("DD-MM-YY")
+    );
+    setDayEvents(events);
+  }, [filteredEvents, day]);
+
+  function getCurrentDayClass() {
+    return day.format("DD-MM-YY") === dayjs().format("DD-MM-YY")
+      ? "bg-blue-600 text-white rounded-full w-7"
+      : "bg-[#d6e1f5] text-black rounded-full w-7";
+  }
+  return (
+    <div className="border border-gray-200 flex flex-col shadow-md shadow-[#d6e3fc] duration-500">
+      <header className="flex flex-col items-center mt-2 hover:scale-110 ">
+        {rowIdx === 0 && (
+          <p className="text-sm">
+            {day.format("ddd").toUpperCase()}
+          </p>
+        )}
+        <p
+          className={`text-sm p-1 mt-4 text-center  ${getCurrentDayClass()}`}
+        >
+          {day.format("DD")}
+        </p>
+      </header>
+      <div
+        className="flex-1 cursor-pointer"
+        onClick={() => {
+          setDaySelected(day);
+          setShowEventModal(true);
+        }}
+      >
+        {dayEvents.map((evt, idx) => (
+          <div
+            key={idx}
+            onClick={() => setSelectedEvent(evt)}
+            className={`bg-${evt.label}-200 p-1 mr-3 text-gray-600 text-sm rounded mb-1 truncate`}
+          >
+            {evt.title}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
